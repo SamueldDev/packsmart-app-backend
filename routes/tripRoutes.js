e873b743e528cd8d2e23dfa90e9ebd00ec20d8dc
SamueldDev <youngsammy2018@gmail.com>
2025-06-03 20:40:33 +0100

    added trip-calender ui

--- routes/tripRoutes.js
+++ routes/tripRoutes.js
@@ -4,11 +4,12 @@ import express from "express"
 
 
 import { createTrip, getTrips } from "../controllers/tripController.js"
+import authenticate from "../middlewares/authMiddleware.js"
 
 const router = express.Router()
 
-router.post("/", createTrip)
-router.get("/", getTrips)
+router.post("/", authenticate, createTrip)
+router.get("/", authenticate, getTrips)
 
 export default router
 
